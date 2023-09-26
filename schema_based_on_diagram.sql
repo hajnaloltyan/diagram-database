36cca00b3b9ff44791a81124393ca22d9f84b3d4
Hajnalka Oltyan <101429702+hajnaloltyan@users.noreply.github.com>
2023-09-27 00:10:05 +0200

    Add required FK indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -46,3 +46,6 @@ CREATE INDEX medical_histories_patient_id_idx ON medical_histories(patient_id);
 CREATE INDEX invoices_medical_history__id_idx ON invoices(medical_history__id);
 CREATE INDEX invoice_items_invoice_id_idx ON invoice_items(invoice_id);
 CREATE INDEX invoice_items_treatment_id_idx ON invoice_items(treatment_id);
+CREATE INDEX medical_history_treatments_medical_history_id_idx ON medical_history_treatments(medical_history_id);
+CREATE INDEX medical_history_treatments_treatment_id_idx ON medical_history_treatments(treatment_id);
+
